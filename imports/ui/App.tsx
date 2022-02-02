9e2841525f7df70a7c6d4b3e1758fb99fdd70331
Mathis Flinois <flinoismathis@gmail.com>
2022-02-02 12:06:51 +0100

    create admin + add tailwind

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -2,6 +2,5 @@ import React from 'react';
 
 export const App = () => (
   <div>
-    <h1>Salut mon ptit pote!</h1>
   </div>
 );
--- server/main.ts
+++ server/main.ts
@@ -1,5 +1,17 @@
 import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
 
 Meteor.startup(() => {
     console.log('SERVER - Server started');
+
+
+    // Admin init
+    const user_admin = Meteor.users.findOne();
+    if(!user_admin){
+        console.log('SERVER - First user generation....');
+        Accounts.createUser({
+            email: "admin@gmail.com",
+            password: "admin"
+        })
+    }
 });
