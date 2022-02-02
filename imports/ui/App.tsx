e4d07209bedd201f2e33063e1a5c72998f9a2034
Mathis Flinois <flinoismathis@gmail.com>
2022-02-02 12:23:40 +0100

    Router

--- imports/ui/App.tsx
+++ imports/ui/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
 
 export const App = () => (
-  <div>
-  </div>
+  <Router>
+    <Routes>
+      <Route path="/" element={<LandingPage/>}/>
+    </Routes>
+  </Router>
 );
